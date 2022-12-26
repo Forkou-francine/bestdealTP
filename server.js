e55eff24ebbbbeab813661f945365c024e2f3747
francine Forkou <100194990+Forkou-francine@users.noreply.github.com>
2022-12-26 13:55:12 +0100

    new commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import path from 'path'
 import { BestOptions, createForfait } from './helper.js'
 import fs from "fs"
 const app = express()
-const port = 3001
+const port = 3000
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
